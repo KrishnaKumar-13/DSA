203e89b26c7277bfe2bade18f2d0aeeb74d15307
krishna kumar <venkatakrishnakumar941@gmail.com>
2026-08-05 20:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (38.04%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+         int start = 0;
+        int end = nums.length-1;
+
+        while(start < end)
+        {
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] > nums[mid+1])
+            {
+                // you are in dec part of array
+                // this may be the ans, but look at left
+                // this is why end != mid - 1
+                end = mid;
+            }
+            else
+            {
+               // you are in asc part of array
+                start = mid + 1; // because we know that mid+1 element > mid element
+            }
+        }
+         // in the end, start == end and pointing to the largest number because of the 2 checks above
+        // start and end are always trying to find max element in the above 2 checks
+        // hence, when they are pointing to just one element, that is the max one because that is what the checks say
+        // we can also return end also because two are pointing to the same element..
+        return start;
+    }
+}
