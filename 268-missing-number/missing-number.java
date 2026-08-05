b8059f224381be0d78c2fc0661dad2ba0bd58524
krishna kumar <venkatakrishnakumar941@gmail.com>
2026-08-05 20:52:09 +0530

    Time: 1 ms (29.74%) | Memory: 47.4 MB (32.99%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        // i am using the cycle sort [o,n]
+        int i =0;
+        while(i<nums.length)
+        {
+            int correct = nums[i];
+            if(nums[i] < nums.length && nums[i] != nums[correct])
+            {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        // to find the missing number
+            for(int index = 0;index <nums.length;index++)
+            {
+                if(nums[index]!= index)
+                {
+                   return index;
+                }
+            } 
+            return nums.length;
+    }
+}
